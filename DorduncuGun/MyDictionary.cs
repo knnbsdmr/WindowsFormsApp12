587440a1109fbf19c9ff3aa1fa1709eaf8e75678
Kenan BAŞDEMİR <kenan.basdemir5858@gmail.com>
2021-01-23 20:12:06 +0300

    Add project files.

---
+++ DorduncuGun/MyDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DorduncuGun
+{
+    class MyDictionary<T1, T2>
+    {
+        private Dictionary<T1, T2> dictionary;
+
+        public MyDictionary()
+        {
+            dictionary = new Dictionary<T1, T2>();
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            dictionary.Add(key, value);
+        }
+    }
+}
---
+++ DorduncuGun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DorduncuGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
